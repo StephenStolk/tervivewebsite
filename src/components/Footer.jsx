a16df4cac98eb36df492365f91caffbb46662bd2
StephenStolk <shikharburman2@gmail.com>
2024-09-06 20:43:32 +0530

    Updated Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-green-600 text-white py-4">
+    <footer className="bg-[#8a9d68] text-white py-4">
       <div className="container mx-auto text-center">
         <p>&copy; 2024 Tervive. All rights reserved.</p>
       </div>
