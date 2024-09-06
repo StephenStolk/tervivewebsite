d7858f3b297ad9d0675d942bfd105a6d096453e9
StephenStolk <shikharburman2@gmail.com>
2024-09-06 20:44:21 +0530

    Updated Home screens

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,15 +5,23 @@ import ProblemSection from './Problem'
 import SolutionSection from './Solution'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import Intro from './Intro'
+import Newsletter from './Newsletter'
+import Services from './Services'
+import TestimonialSection from './Testimonial'
 
 const Home = () => {
   return (
     <>
     <Navbar />
+    <Intro />
+    <Newsletter />
+    <Services/>
     <HeroSection />
     <ProblemSection/>
     <SolutionSection />
-    
+    <TestimonialSection />
+    <Footer />
     </>
   )
 }
